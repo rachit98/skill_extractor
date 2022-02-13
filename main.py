3b12d183f856ac8af4eebb8c8132cff76aaa651f
rachit98 <rachity@gmail.com>
2022-02-13 22:55:33 +0530

    Modify: Modified punctuation code and added JD

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from pdfminer.high_level import extract_text
 import nltk
+import string
 
-
+# Correspond to question tags field
+# need not be manually coded can be inputed from inventory.csv
 TECH_SKILLS_DB = [
     'java',
-    'c++',
+    'c',
     'javascript',
     'machinelearning',
     'linux',
     'mongodb',
-    'python'
+    'python',
+    'oops',
+    'unix'
 ]
 
+# Corresponding to questions for HR round 
 SOFT_SKILLS_DB = [
     'honesty',
     'integrity',
@@ -23,11 +28,12 @@ SOFT_SKILLS_DB = [
     'discipline'
 ]
 
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
+# downloading word dictionaries, will be useful when word2vec is implemented
+nltk.download('stopwords', quiet=True)
+nltk.download('punkt', quiet=True)
+nltk.download('averaged_perceptron_tagger', quiet=True)
+nltk.download('maxent_ne_chunker', quiet=True)
+nltk.download('words', quiet=True)
 
 def extract_text_from_pdf(pdf_path):
     return extract_text(pdf_path)
@@ -41,13 +47,16 @@ def extract_tech_skills(input_text):
     # removing the stop words
     filtered_tokens = [w for w in word_tokens if w not in stop_words]
 
-    # remove punctuations
+    # remove punctuations (Need to check this one)
+    # filtered_tokens = [w for w in word_tokens if w.isalpha()]
+    filtered_tokens = str.maketrans(string.punctuation, ' '*len(string.punctuation))
     filtered_tokens = [w for w in word_tokens if w.isalpha()]
+    
 
     # Generating bigrams (Machine Learning --> MachineLearning)
     bigrams_trigrams = list(map(' '.join, nltk.everygrams(filtered_tokens, 2, 3)))
 
-    store_skills = set()
+    store_skills = set() #Returning a set, can be converted into list/JSON/P-Queue/other
 
     for token in filtered_tokens:
         if token.lower() in TECH_SKILLS_DB:
@@ -61,5 +70,5 @@ def extract_tech_skills(input_text):
 
 
 if __name__=='__main__':
-    text = extract_text_from_pdf('./resumes/rachit.pdf')
-    print('Soft Skills : ',extract_tech_skills(text))
+    text = extract_text_from_pdf('./JD/MS_JD.pdf') # Can be autoupdated based on Candidate id/ Job id
+    print('Tech Skills : ',extract_tech_skills(text))
